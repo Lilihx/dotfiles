431371ee9181f22ac71ef9b2f92b83916e727321
lihengxin <lihengxin.123@bytedance.com>
2024-12-16 17:42:24 +0800

    feat: update nvim
    Change-Id: I1a21614523733f539e616bc5721e14e40453e6e2

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "gopls", "pylsp", "rust_analyzer" }
+local servers = { "html", "cssls", "gopls", "pylsp", "rust_analyzer", "lua_ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -14,3 +14,33 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.lua_ls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        version = "Lua 5.4",
+        path = {
+          "?.lua",
+          "?/init.lua",
+          vim.fn.expand "~/.luarocks/share/lua/5.4/?.lua",
+          vim.fn.expand "~/.luarocks/share/lua/5.4/?/init.lua",
+          "/usr/local/share/5.4/?.lua",
+          "/usr/local/share/lua/5.4/?/init.lua",
+        },
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          vim.fn.expand "~/.luarocks/share/lua/5.4",
+          "/usr/local/share/lua/5.4",
+        },
+      },
+    },
+  },
+}
